991f0c4d85ac22e6e03d92f7e1727151a6eac974
CooperKe <keice5@hotmail.com>
2015-03-13 00:42:40 +0800

    Fibonacci, the iterative version

--- lab-1i/fib.s
+++ lab-1i/fib.s
@@ -14,8 +14,8 @@ fibonacci:
 	push {r3, r4, r5, r6, lr}
 
 	@ r3(n1), r4(n2), r5(sum), r6(i)
-	mov r3, #-1		@ r3 == n1  
-	mov r4, #1		@ r4 == n2
+	mov r3, #-1		@ n1 = -1 (previous) 
+	mov r4, #1		@ n2 = 1 (result) 
 	mov r5, #0		@ r5 == sum
 	mov r6, #0		@ r6 == i
 .loop:
@@ -24,8 +24,7 @@ fibonacci:
 	mov r4, r5		@ n2 = sum
 	add r6, r6, #1		@ i = i + 1
 	
-	CMP r0, r6 		@ x-i>0 means i < x
-	it ge			@ if i <= x
+	CMP r0, r6 		@ if(i <= x) (x-i=>0 means i<=x)
 	bge .loop		@ next cycle
 
 	mov r0, r5		@ r0 is the return value
